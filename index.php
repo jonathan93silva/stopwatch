6622f46f130dadf3495dcc613ded3994aad5907f
jonathan93silva <jonathan93silva@gmail.com>
2020-11-24 08:52:39 -0300

    update

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Exo+2:wght@500;700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="shortcut icon" href="imgs/favicon.ico" type="image/x-icon">
+    <title>Stopwatch</title>
+</head>
+<body>
+    <div class="container">
+        <div class="timer">
+            <div class="display">
+                <h1>01</h1>
+                <h2>Hours</h2>
+            </div>
+            <div class="display">
+                <h1>26</h1>
+                <h2>Minutes</h2>
+            </div>
+            <div class="display">
+                <h1>47</h1>
+                <h2>Seconds</h2>
+            </div>
+        </div>
+        <div class="controller"></div>
+    </div>
+</body>
+</html>
