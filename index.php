c8d5cdfcd1fcd62b8bd07813dcfe278ec63449a4
jonathan93silva <jonathan93silva@gmail.com>
2020-12-01 22:30:42 -0300

    updatte

--- index.php
+++ index.php
@@ -4,30 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Exo+2:wght@500;700&display=swap" rel="stylesheet">
-
     <link rel="stylesheet" href="styles/style.css">
     <link rel="shortcut icon" href="imgs/favicon.ico" type="image/x-icon">
+
     <title>Stopwatch</title>
 </head>
 <body>
     <div class="container">
-        <div class="timer">
-            <div class="display">
-                <h1>01</h1>
-                <h2>Hours</h2>
-            </div>
-            <div class="display">
-                <h1>26</h1>
-                <h2>Minutes</h2>
-            </div>
-            <div class="display">
-                <h1>47</h1>
-                <h2>Seconds</h2>
-            </div>
+        <div class="check-milliseconds">
+            <label>
+            <input type="checkbox">
+            Mostrar millisegundos
+            </label>
         </div>
-        <div class="controller"></div>
+        <div class="display">
+            <h1>01:32:20.0<label>00</label></h1>
+        </div>
+        <div class="controller">
+            <button class="btn">Iniciar</button>
+            <button class="btn">Voltar</button>
+            <button class="btn">Reiniciar</button>
+        </div>
     </div>
 </body>
 </html>
